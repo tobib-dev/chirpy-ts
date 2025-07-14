fca0eeff4e33d8fcc4d774e96f2e7f4b7082c4a7
tobi <tobi19.py@gmail.com>
2025-07-14 13:09:01 -0500

    Create readiness.js

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import express, { Request, Response } from "express";
-import path from "path";
+import express from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
-
-function handlerReadiness(req: Request, res: Response) {
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send("OK");
-}
 
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
