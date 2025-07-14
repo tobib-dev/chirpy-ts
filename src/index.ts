ec4f4f74841b25cd59beb68e68e1f51380319807
tobi <tobi19.py@gmail.com>
2025-07-14 13:01:09 -0500

    Serve from /app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+function handlerReadiness(req: Request, res: Response) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
+
+app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
