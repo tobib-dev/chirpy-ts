ce03ea46c62833b2d4dc6ca0162e35c72682f3d9
tobi <tobi19.py@gmail.com>
2025-08-16 10:49:36 -0500

    CHORE: Implement create chirp endpoint

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { respondWithJSON } from "./json.js";
 import { BadRequestError } from "./errors.js";
+import { createChirp } from "../db/queries/chirps.js";
+import { createDeflate } from "node:zlib";
 
 export async function handlerValidateChirps(req: Request, res: Response) {
   type parameters = {
@@ -28,5 +30,35 @@ export async function handlerValidateChirps(req: Request, res: Response) {
 
   respondWithJSON(res, 200, {
     cleanedBody: newBody,
+  });
+}
+
+export async function handlerCreateChirp(req: Request, res: Response) {
+  type Parameters = {
+    body: string;
+    userId: string;
+  };
+
+  const params: Parameters = req.body;
+
+  if (!params.body) {
+    throw new BadRequestError("Missing required fields: body");
+  }
+  if (!params.userId) {
+    throw new BadRequestError("Missing required fields: userId");
+  }
+
+  const chirp = await createChirp({ body: params.body, userId: params.userId });
+
+  if (!chirp) {
+    throw new Error("Could not create chirp");
+  }
+
+  respondWithJSON(res, 201, {
+    id: chirp.id,
+    body: chirp.body,
+    userId: chirp.userId,
+    createdAt: chirp.createdAt,
+    updatedAt: chirp.updatedAt,
   });
 }
