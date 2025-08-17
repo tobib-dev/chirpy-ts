ff96d047a33d23f468e9e94b76a4f5c2944de8c6
tobi <tobi19.py@gmail.com>
2025-08-17 10:01:13 -0500

    CHORE: Refactor query result variables

--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -3,8 +3,8 @@ import { db } from "../index.js";
 import { NewChirp, chirps } from "../schema.js";
 
 export async function createChirp(chirp: NewChirp) {
-  const [result] = await db.insert(chirps).values(chirp).returning();
-  return result;
+  const [rows] = await db.insert(chirps).values(chirp).returning();
+  return rows;
 }
 
 export async function reset() {
@@ -16,6 +16,9 @@ export async function getChirps() {
 }
 
 export async function getChirp(id: string) {
-  const [result] = await db.select().from(chirps).where(eq(chirps.id, id));
-  return result;
+  const rows = await db.select().from(chirps).where(eq(chirps.id, id));
+  if (rows.length === 0) {
+    return;
+  }
+  return rows[0];
 }
