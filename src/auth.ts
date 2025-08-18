145994b39df3ecbbe251f346949ab381185d6759
tobi <tobi19.py@gmail.com>
2025-08-18 15:48:22 -0500

    CHORE: Create JWT for api login

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
 export async function hashPassword(password: string) {
   return bcrypt.hashSync(password, 10);
@@ -6,4 +9,14 @@ export async function hashPassword(password: string) {
 
 export async function checkPasswordHash(password: string, hash: string) {
   return bcrypt.compare(password, hash);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string) {
+  const load: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + expiresIn,
+  };
+  return jwt.sign(load, secret, { expiresIn });
 }
