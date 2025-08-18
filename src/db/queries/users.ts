63e65d2f89c0264e1b5da42a85a0c47885d0e523
tobi <tobi19.py@gmail.com>
2025-08-18 14:19:36 -0500

    CHORE: Query user by email

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,5 +1,6 @@
 import { db } from "../index.js";
 import { NewUser, users } from "../schema.js";
+import { eq } from "drizzle-orm";
 
 export async function createUser(user: NewUser) {
   const [result] = await db
@@ -12,4 +13,12 @@ export async function createUser(user: NewUser) {
 
 export async function reset() {
   await db.delete(users);
+}
+
+export async function getUserByEmail(email: string) {
+  const rows = await db.select().from(users).where(eq(users.email, email));
+  if (rows.length === 0) {
+    return;
+  }
+  return rows[0];
 }
