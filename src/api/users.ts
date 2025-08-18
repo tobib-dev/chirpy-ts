3a66d38e9f60a86831fb27a04899a4df883a304c
tobi <tobi19.py@gmail.com>
2025-08-18 14:12:49 -0500

    CHORE: Hash password and store in db during accoutn creation

--- src/api/users.ts
+++ src/api/users.ts
@@ -3,26 +3,51 @@ import { createUser } from "../db/queries/users.js";
 import { NewUser } from "../db/schema.js";
 import { respondWithError, respondWithJSON } from "./json.js";
 import { BadRequestError } from "./errors.js";
+import { hashPassword } from "./auth.js";
 
 export async function handlerCreateUser(req: Request, res: Response) {
   type Parameters = {
     email: string;
+    password: string;
   };
+
+  type User = {
+    id: string;
+    email: string;
+    hashedPassword: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+
+  type UserResponse = Omit<User, "hashedPassword">;
+
   const params: Parameters = req.body;
-  if (!params.email) {
+  if (!params.email || !params.password) {
     throw new BadRequestError("Missing required fields");
   }
 
-  const user = await createUser({ email: params.email });
+  const hashedPassword = hashPassword(params.password);
+
+  if (!hashedPassword) {
+    respondWithError(res, 500, "Failed to hash password");
+    return;
+  }
+
+  const user = await createUser({
+    email: params.email,
+    hashedPassword: hashedPassword,
+  });
 
   if (!user) {
     throw new Error("Couldn't create user");
   }
 
-  respondWithJSON(res, 201, {
+  const userResponse: UserResponse = {
     id: user.id,
+    email: user.email,
     createdAt: user.createdAt,
     updatedAt: user.updatedAt,
-    email: user.email,
-  });
+  };
+
+  respondWithJSON(res, 201, userResponse);
 }
