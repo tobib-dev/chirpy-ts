89321edc2a5151f6eae6620861b56d7502eae24f
tobi <tobi19.py@gmail.com>
2025-08-20 16:10:49 -0500

    CHORE: Store refresh token during login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,18 +3,19 @@ import { getUserByEmail } from "../db/queries/users.js";
 import { UnauthorizedError } from "./errors.js";
 import { respondWithJSON } from "./json.js";
 import { UserResponse } from "./users.js";
-import { checkPasswordHash, makeJWT } from "../auth.js";
+import { checkPasswordHash, makeJWT, makeRefreshToken } from "../auth.js";
 import { config } from "../config.js";
+import { createRefreshToken } from "src/db/queries/refresh_tokens.js";
 
 type LoginResponse = UserResponse & {
   token: string;
+  refreshToken: string;
 }
 
 export async function handlerLogin(req: Request, res: Response) {
   type parameter = {
     password: string;
     email: string;
-    expiresIn?: number
   };
 
   const params: parameter = req.body;
@@ -32,16 +33,21 @@ export async function handlerLogin(req: Request, res: Response) {
   }
 
   let duration = config.jwt.defaultDuration;
-  if (params.expiresIn && !(params.expiresIn > config.jwt.defaultDuration)) {
-    duration = params.expiresIn;
-  }
   const accessToken = makeJWT(user.id, duration, config.jwt.secret);
+  const refreshToken = makeRefreshToken(accessToken);
+
+  const dbToken = await createRefreshToken({
+    token: refreshToken,
+    userId: user.id,
+    expiresAt: new Date(Date.now() + (duration * 1000)),
+  })
 
   respondWithJSON(res, 200, {
     id: user.id,
     email: user.email,
     createdAt: user.createdAt,
     updatedAt: user.updatedAt,
-    token: accessToken
+    token: accessToken,
+    refreshToken: refreshToken
   } satisfies LoginResponse);
 }
