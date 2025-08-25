61cfa5c3b1876bedf3c4b599bf42d62c11902311
tobi <tobi19.py@gmail.com>
2025-08-24 19:14:12 -0500

    CHORE: Add query upgrade user to chirpy red

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -32,3 +32,10 @@ export async function updateUser(id: string, email: string, password: string) {
     .returning();
   return result;
 }
+
+export async function upgradeUser(id: string) {
+  const [result] = await db.update(users)
+    .set({ isChirpyRed: true})
+    .where(eq(users.id, id)).returning();
+  return result;
+}
